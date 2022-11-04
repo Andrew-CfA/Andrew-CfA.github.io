cad1ed5428c7c9af8947c16f4460302f57347f63
Andrew Hernandez <ahernandez@codeforamerica.org>
2022-11-03 23:09:35 -0700

    sorting done

--- tabularDataView/script.js
+++ tabularDataView/script.js
@@ -1,77 +1,19 @@
-fetch("https://data.cityofnewyork.us/resource/au7q-njtk.json")
-.then(function(response){
-return response.json();
-})
-.then(function(datas){
-let placeholder = document.querySelector("#dataOutput");
-let out = "";
-for(let data of datas){
-    out += `
-        <tr>
-            <td>${data.objectid}</td>
-            <td>${data.boro_name}</td>
-            <td>${data.assetsubty}</td>
-            <td>${data.date_inst}</td>
-        </tr>
-    `;
-}
-placeholder.innerHTML = out;
+$.ajax({
+    type     : "GET",
+    url      : 'https://data.cityofnewyork.us/resource/au7q-njtk.json',
+    dataType : 'json',
+    success  : function(data) {
+
+        data.forEach(function(post) {
+            var td1 = $('<td />', {html : post.objectid}),
+                td2 = $('<td />', {html : post.boro_name}), 
+                td3 = $('<td />', {html : post.assetsubty}),
+                td4 = $('<td />', {html : post.date_inst}),
+                tr  = $('<tr />');
+            $( '#bikeData' ).append( tr.append(td1, td2, td3, td4) );
+        });
+        let table = new DataTable('#bikeData');
+    }
 });
 
 
-
-function sortTable(n) {
-var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-table = document.getElementById("bikeData");
-switching = true;
-// Set the sorting direction to ascending:
-dir = "asc";
-/* Make a loop that will continue until
-no switching has been done: */
-while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-    // Start by saying there should be no switching:
-    shouldSwitch = false;
-    /* Get the two elements you want to compare,
-    one from current row and one from the next: */
-    x = rows[i].getElementsByTagName("TD")[n];
-    y = rows[i + 1].getElementsByTagName("TD")[n];
-    /* Check if the two rows should switch place,
-    based on the direction, asc or desc: */
-        if (dir == "asc") {
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-            }
-        } else if (dir == "desc") {
-            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-            }
-        }
-    }
-        if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        // Each time a switch is done, increase this count by 1:
-        switchcount ++;
-        setTimeout(shouldSwitch, 10000)
-        } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
-        if (switchcount == 0 && dir == "asc") {
-            dir = "desc";
-            switching = true;
-        }
-    }
-}
-}
