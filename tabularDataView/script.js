d6d2400b9487bc731c88dd6deb0244418f66a1a4
Andrew Hernandez <ahernandez@codeforamerica.org>
2022-11-03 23:35:48 -0700

    included address

--- tabularDataView/script.js
+++ tabularDataView/script.js
@@ -7,10 +7,11 @@ $.ajax({
         data.forEach(function(post) {
             var td1 = $('<td />', {html : post.objectid}),
                 td2 = $('<td />', {html : post.boro_name}), 
-                td3 = $('<td />', {html : post.assetsubty}),
-                td4 = $('<td />', {html : post.date_inst}),
+                td3 = $('<td />', {html : post.ifoaddress}), 
+                td4 = $('<td />', {html : post.assetsubty}),
+                td5 = $('<td />', {html : post.date_inst}),
                 tr  = $('<tr />');
-            $( '#bikeData' ).append( tr.append(td1, td2, td3, td4) );
+            $( '#bikeData' ).append( tr.append(td1, td2, td3, td4, td5,) );
         });
         let table = new DataTable('#bikeData');
     }
